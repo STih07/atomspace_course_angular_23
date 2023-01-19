8671471ad78cae265d8134f5b9b0b423f00907fe
Serhii T <stih07@serhiitkhovskiy.speedport.ip>
2023-01-19 17:55:17 +0100

    lection 19/01/23 + previous

--- src/app/pipes/multiplier.pipe.ts
+++ src/app/pipes/multiplier.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import {interval, Observable, timer} from "rxjs";
 
 @Pipe({
   name: 'multiplier'
 })
 export class MultiplierPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number, arg2: any): Observable<any> {
+    return interval(value * arg2);
+    // return '10';
   }
 
 }
